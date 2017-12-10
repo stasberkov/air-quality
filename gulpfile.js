b6a71f7dabd558154be26246798578f4bc787b22
Stas Berkov <sberkov@cqg.com>
2017-12-10 23:16:22 +0400

    Use gulp tasks to prepair code for espruino and run espruino console

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,57 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
-var esp = require("espruino");
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+const fs = require("fs");
+const { fork } = require('child_process');
+const path = require("path");
+
+const distDir = "./dist";
+const espReadyBundleFileName = "bundle.js";
+const espReadyBundlePath = path.join(distDir, espReadyBundleFileName);
+const appFileName = "app.js";
+const espConsoleBeingWatchedFileName = "esp-console-input.js";
+const espConsoleBeingWatchedFilePath = path.join(distDir, espConsoleBeingWatchedFileName);
+
+gulp.task("build", ["prepare-for-espruino"]);
+
+gulp.task("prepare-for-espruino", ['compile-ts', 'content-to-dist'], (cb) => {
+    const buildproc = fork(
+        require.resolve("espruino/bin/espruino-cli"),
+        ["--board", "ESP32", appFileName, "-o", espReadyBundleFileName],
+        { cwd: distDir });
+    buildproc.on('close', (code) => {
+        cb();
+    });
+});
 
 gulp.task("compile-ts", function () {
-    var tsResult =
-        tsProject.src()
-            .pipe(tsProject());
-            
-
-    return tsResult.js.pipe(gulp.dest('./dist'));
+    const tsResult = tsProject.src().pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest(distDir));
 });
 
-gulp.task("copy-to-dist", () => {
+gulp.task("content-to-dist", () => {
     return gulp
         .src("src/**/*.js", { base: 'src' })
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest(distDir));
 });
 
-gulp.task("app-to-dist",  ['compile-ts', 'copy-to-dist']);
+gulp.task("send-to-espurino-console", (cb) => {
+    const content = fs.readFileSync(espReadyBundlePath);
+    fs.writeFile(
+        espConsoleBeingWatchedFilePath,
+        content,
+        (err) => {
+            if (err) { throw err; }
+            cb();
+        });
+});
+
+gulp.task("espruino-console", ["send-to-espurino-console"], (cb) => {
+    const buildproc = fork(
+        require.resolve("espruino/bin/espruino-cli"),
+        ["--board", "ESP32", "-b", "115200", "--port", "COM3", "-w", espConsoleBeingWatchedFileName],
+        { cwd: distDir });
+    buildproc.on('close', (code) => {
+        cb();
+    });
+});
